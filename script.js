b719e8cea6a6b7fc5b3dc001b16be63dacfcaabc
Carlos Henrique <carlos.c.r.w@gmail.com>
2023-01-19 22:40:43 -0300

    aula 03

--- script.js
+++ script.js
@@ -2,7 +2,18 @@ const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
 
 const data = {
-  run: ["01-01", "01-02", "01-06", "01-08", "01-09"],
+  run: [
+    "01-01",
+    "01-02",
+    "01-06",
+    "01-08",
+    "01-09",
+    "01-10",
+    "01-11",
+    "01-12",
+    "01-13",
+    "01-14",
+  ],
   water: ["01-04", "01-05"],
   food: ["01-01", "01-03"],
   medicine: ["01-02"],
