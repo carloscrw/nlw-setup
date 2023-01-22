4de1855773f39f3fd5fa5e810f9c375cffb0c877
Carlos Henrique <carlos.c.r.w@gmail.com>
2023-01-22 09:23:07 -0300

    aula 04

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const form = document.querySelector("form");
 const nlwSetup = new NLWSetup(form);
+const button = document.querySelector("header button");
 
-const data = {
-  run: [
-    "01-01",
-    "01-02",
-    "01-06",
-    "01-08",
-    "01-09",
-    "01-10",
-    "01-11",
-    "01-12",
-    "01-13",
-    "01-14",
-  ],
-  water: ["01-04", "01-05"],
-  food: ["01-01", "01-03"],
-  medicine: ["01-02"],
-};
+button.addEventListener("click", add);
+form.addEventListener("change", save);
 
+function add() {
+  const tody = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  const dayExists = nlwSetup.dayExists(tody);
+  if (dayExists) {
+    alert("Dia já incluso!🔴");
+    return;
+  }
+  alert("Adiconado com sucesso!✅");
+  nlwSetup.addDay(tody);
+}
+function save() {
+  localStorage.setItem("NLWSetup@habtis", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup@habtis")) || {};
 nlwSetup.setData(data);
 nlwSetup.load();
